5343c6e0f9a0520087e27284579921bd708a6cf9
MishC <michaela.chovancova@bergen.kommune.no>
2026-03-25 05:33:10 +0100

    register redis services Cart and Catalog

--- OrderService/Program.cs
+++ OrderService/Program.cs
@@ -13,7 +13,18 @@ builder.Services.AddStackExchangeRedisCache(options =>
     options.InstanceName = "order-service:";
 });
 
+builder.Services.AddHttpClient("CartApi", client =>
+{
+    client.BaseAddress = new Uri("http://cartservice:8080"); //http://localhost:5002
+});
+
+builder.Services.AddHttpClient("CatalogApi", client =>
+{
+    client.BaseAddress = new Uri("http://catalogservice:8080"); //http://localhost:5001
+});
+
 builder.Services.AddSingleton<OrderStore>();
+
 var app = builder.Build();
 
 app.UseSwagger();
